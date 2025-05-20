c29bb3f3cc5225a917d1651bd2bedea2012e469b
Mo Ngai <mo.ngai@nexera-robotics.com>
2025-05-20 10:03:59 -0700

    meanshift v2

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from pathlib import Path
 
@@ -6,19 +7,40 @@ import matplotlib.pyplot as plt
 import torch
 from depth_anything_v2.dpt import DepthAnythingV2
 from nexera_packages.utilities.image_processing import *
-from sklearn.cluster import MeanShift
+from sklearn.cluster import DBSCAN, MeanShift
 from tqdm import tqdm
 
+# these colors are in BGR format
+predefined_colors = [
+    [0, 0, 255],    # Red
+    [0, 255, 0],    # Green
+    [255, 0, 0],    # Blue
+    [0, 255, 255],  # Yellow
+    [255, 0, 255],  # Magenta
+    [255, 255, 0]   # Cyan
+]
 
 def main():
     source_dir = r'C:\Users\Yankee\Documents\nexera\nexera-core\Depth-Anything-V2\assets\inputs'
+    output_dir = r'C:\Users\Yankee\Documents\nexera\nexera-core\Depth-Anything-V2\assets\outputs'
+
     DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+
+    interested_surface = 5
 
     model_configs = {
         'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
         'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
         'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
         'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
+    }
+
+    flat_model = {
+        "MeanShift": MeanShift(bandwidth=0.1, bin_seeding=True),
+        # "DBSCAN": DBSCAN(eps=0.3, min_samples=100, algorithm='ball_tree'),
+        # "KMeans": KMeans(n_clusters=interested_surface, random_state=42),
+        # "Agglomerative": AgglomerativeClustering(n_clusters=interested_surface),
+        # "GMM": GaussianMixture(n_components=interested_surface, covariance_type='full', random_state=42)
     }
 
     encoder = 'vitl' # or 'vits', 'vitb', 'vitg'
@@ -37,8 +59,12 @@ def main():
 
         name = file.name
 
-        cv2.imwrite(f'outputs/normals/normal-{name}', nmap)
-        cv2.imwrite(f'outputs/depths/depth-{name}', dmap)
+        os.makedirs(f'{output_dir}/normals/', exist_ok = True)
+        os.makedirs(f'{output_dir}/depths/', exist_ok = True)
+        os.makedirs(f'{output_dir}/flats', exist_ok = True)
+
+        cv2.imwrite(f'{output_dir}/normals/normal-{name}', nmap)
+        cv2.imwrite(f'{output_dir}/depths/depth-{name}', dmap)
 
         # Reshape normal map for clustering
         H, W, _ = nmap.shape
@@ -48,23 +74,24 @@ def main():
         norms = np.linalg.norm(normals, axis=1, keepdims=True)
         normals_normalized = normals / (norms + 1e-8)
 
-        # Mean Shift clustering on normal vectors
-        ms = MeanShift(bandwidth=0.1, bin_seeding=True)
-        labels = ms.fit_predict(normals_normalized)
-        labels_image = labels.reshape(H, W)
+        for m_name, flat_clustering_model in flat_model.items():
+            labels = flat_clustering_model.fit_predict(normals_normalized)
+            labels_image = labels.reshape(H, W)
 
-        # Find the largest cluster (most common label)
-        unique_labels, counts = np.unique(labels, return_counts=True)
-        largest_label = unique_labels[np.argmax(counts)]
+            # Find all the clusters
+            unique_labels, counts = np.unique(labels, return_counts=True)
+            sort_counts = np.argsort(-counts)
 
-        # Create a mask for the largest flat region
-        flat_mask = (labels_image == largest_label).astype(np.uint8)
+            overlay = np.zeros((H, W, 3), dtype=np.uint8)
 
-        # Create a red overlay (RGB image)
-        overlay = np.zeros((H, W, 3), dtype=np.uint8)
-        overlay[flat_mask == 1] = [0, 0, 255]  # BGR
+            for i in sort_counts[:interested_surface]:
+                interested_label = unique_labels[i]
 
-        cv2.imwrite(f'outputs/flats/flat-{name}', overlay)
+                flat_mask = (labels_image == interested_label).astype(np.uint8)
+
+                overlay[flat_mask == 1] = predefined_colors[i]
+
+            cv2.imwrite(f'{output_dir}/flats/{m_name}-{name}', overlay)
     
 if __name__ == "__main__":
     main()
